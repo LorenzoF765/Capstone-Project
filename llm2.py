491d4f2aee9686af762026c08a71329b2d9124b2
Lorenzo Franco <lfranco@student.neumont.edu>
2024-04-03 14:23:27 -0600

    My life is HuggingFace  Documentation

--- llm2.py
+++ llm2.py
@@ -147,3 +147,6 @@ while True:
     output = model.generate(input_ids, max_length=100, num_return_sequences=1, no_repeat_ngram_size=2, top_k=50, top_p=0.95, temperature=0.7)
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     print(generated_text)
+
+
+#UGHHHHHHHHHH
